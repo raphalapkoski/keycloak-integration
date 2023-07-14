9c2152eec99b321b7a2067695874b9fe8440ed22
Raphael Lapkoski Mazanek <raphael.estudo@outlook.com>
2023-07-14 00:33:05 -0300

    Initial commit.

---
+++ src/04 - Infra/KeycloakIntegration.Infra/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace KeycloakIntegration.Infra
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfra(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
