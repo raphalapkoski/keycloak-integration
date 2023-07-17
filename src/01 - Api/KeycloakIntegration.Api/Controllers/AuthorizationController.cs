368a5a9109a16656c462eb5850f974db0cb820e5
Raphael Lapkoski Mazanek <raphael.estudo@outlook.com>
2023-07-17 15:38:40 -0300

    Authorized implementation

---
+++ src/01 - Api/KeycloakIntegration.Api/Controllers/AuthorizationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KeycloakIntegration.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            return Ok("Autorizado");
+        }
+    }
+}
--- src/01 - Api/KeycloakIntegration.Api/Program.cs
+++ src/01 - Api/KeycloakIntegration.Api/Program.cs
@@ -1,6 +1,8 @@
 using KeycloakIntegration.Application;
 using KeycloakIntegration.Application.Middleware;
 using KeycloakIntegration.Infra;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,9 +16,24 @@ builder.Services.AddApplication();
 
 builder.Services.AddInfra();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, o =>
+                {
+                    o.MetadataAddress = builder.Configuration.GetValue<string>("Keycloak:metadata-address-url");
+                    o.Authority = builder.Configuration.GetValue<string>("Keycloak:authority-server-url");
+                    o.RequireHttpsMetadata = builder.Configuration.GetValue<bool>("Keycloak:require-https-metadata");
+
+                    o.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = builder.Configuration.GetValue<bool>("Keycloak:validate-token-audience"),
+                        ValidateLifetime = builder.Configuration.GetValue<bool>("Keycloak:validate-token-lifetime"),
+                        ValidAudience = builder.Configuration.GetValue<string>("Keycloak:audience")
+                    };
+                });
+
 builder.Services.AddHttpClient("keycloak", client =>
 {
-    client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("Keycloak:authentication-url"));
+    client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("Keycloak:base-url"));
 });
 
 var app = builder.Build();
@@ -30,6 +47,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
